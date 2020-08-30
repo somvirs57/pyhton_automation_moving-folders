62a9e051ae4766243674bee529d77ee3531d2569
somvirs57 <61090831+somvirs57@users.noreply.github.com>
2020-08-30 20:22:42 +0530

    some bugs fixed
    Fixed bugs that were present in last commit:
    1. only empty files were moved using shutil.move so added copyfunction=shutil.copytree to move folder and all of its content
    2. as it was running only once per day as per my requirement and if no changes are made at that exact time of script running then there would be no move of folders. So, added a method test_file_touch in observer class to touch the file.test folder to imitate that the folder is being updated in the directory.
    3. modified the Handler class event from on_modified to on_any_event because we just want to do some changes in folder to initiate it.

--- folder_automation.py
+++ folder_automation.py
@@ -3,6 +3,9 @@ import os
 import shutil
 from watchdog.observers import Observer 
 from watchdog.events import FileSystemEventHandler 
+#new import to modify a file at the time of program run
+from pathlib2 import Path
+
 
 class OnMyWatch:
     
@@ -13,34 +16,59 @@ class OnMyWatch:
     def run(self):
         event_handler = Handler() 
         
-        self.observer.schedule(event_handler, source_dir, recursive=True)
+        self.observer.schedule(event_handler, source_dir, recursive=False)
         self.observer.start()
+        self.check_file_touch()
         
         try:
-            while True:
-                time.sleep(10)
+            '''
+            adding this code of line to stop observer after 5 minutes 
+            '''
+            time.sleep(300)
+            self.observer.stop()
+            print("observer stopped")
         except: 
             self.observer.stop()
             print("observer stopped")
             
         self.observer.join()
-        
+    '''
+    Added this new method (check_file_touch) as i am running this program once in a day 
+    and there might be no changes at that exact time. 
+    
+    so this method will change the update time of the directory or create one if not exists
+    so that the program can know that there are some changes made and it can run
+    '''
+    @staticmethod    
+    def check_file_touch():
+        file_name = 'file.test'
+        file_directory = os.path.join(source_dir, file_name)
+        file_exists = os.path.exists(file_directory)
+        if file_exists:
+            Path(file_directory).touch()
+        else:
+            os.mkdir(file_directory)
 
 class Handler(FileSystemEventHandler):
-
-    def on_modified(self, event):
+    
+    def on_any_event(self, event):
         # necessary to put the folder name as (example - destination), this will determine where the 
         # folder will be sent
         #check if we get folder
         if event.is_directory:
             for folder in os.listdir(source_dir):
-                if folder != 'folder_automation.py':
+                '''
+                added new check statement to check if the folder is not one of the two files
+                necessary to run this program
+                '''
+                if folder != 'folder_automation.py' and folder != 'file.test':
                     # splitting the file name and destination (example - python)
                     # it means we want to send the example folder to python destination
                     file,dest = folder.split('-')
                     file = file.strip()                 # removing the extra white space
                     dest = dest.strip().lower()         # removing extra whitespace and making lowercase
-                
+                    
+                    print(f"{file} should be moved to {dest}")
                     #renaming the folder to required name after removing hyphen(-) from name
                     folder_source = os.path.join(source_dir,file)
                     os.rename(os.path.join(source_dir,folder),folder_source)
@@ -50,8 +78,8 @@ class Handler(FileSystemEventHandler):
                     
                     #calling move_file method that will move the folder to required destination
                     self.move_file(file,folder_source,folder_destination)
-                
-                
+                    
+    
     @staticmethod                
     def move_file(file, source, destination):
         # same file name if the destination folder is empty
@@ -73,17 +101,34 @@ class Handler(FileSystemEventHandler):
         new_dest_name = os.path.join(destination, new_name)
         
         #moving folder from source to destination
-        shutil.move(source,new_dest_name)
+        '''
+        added copy_function = shutil.copytree to move the folder and its complete 
+        contents as previously only empty folders were moved
+        '''
+        shutil.move(source,new_dest_name,copy_function = shutil.copytree)
+        print("moved success")
             
                
 source_dir = r'C:\Users\somvi\Desktop\Automate files'
 
 destination_sources = {"android": r'C:\Users\somvi\Desktop\android studio',
                        "python": r'C:\Users\somvi\Desktop\Python programming'}    
+
+
+def check_file_touch():
+    file_name = 'file.txt'
+    file_directory = os.path.join(source_dir, file_name)
+    file_exists = os.path.exists(file_directory)
+    if file_exists:
+        Path(file_directory).touch()
+    else:
+        os.mkdir(file_directory)
+    
     
 if __name__ == "__main__":
     watch = OnMyWatch()
     watch.run()
+    
     
     
     
